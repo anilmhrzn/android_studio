5db6316c676052949c0fbdadb1e10956c0b3a38d
anilmhrzn <anilmaharjan9995@gmail.com>
2023-04-02 22:06:25 +0545

    register and home page added

--- AsianPay/app/src/main/java/com/example/asianpay/Login.java
+++ AsianPay/app/src/main/java/com/example/asianpay/Login.java
@@ -2,6 +2,7 @@ package com.example.asianpay;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
@@ -14,13 +15,13 @@ public class Login extends AppCompatActivity {
     Button login_btn;
     Button register_btn;
     Button forgot_password_btn;
-    TextInputEditText input_username ;
-    TextInputEditText input_password;
-    String username;
-    String password;
-//    private boolean isValidString(String str) {
-//        return str != null && !TextUtils.isEmpty(str);
-//    }
+    TextInputEditText username ;
+    TextInputEditText password;
+    String input_username;
+    String input_password;
+    private boolean isValidString(String str) {
+        return str != null && !TextUtils.isEmpty(str);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,17 +32,38 @@ public class Login extends AppCompatActivity {
         login_btn=findViewById(R.id.login_btn);
         register_btn=findViewById(R.id.register_btn);
         forgot_password_btn=findViewById(R.id.forgot_password_btn);
+        password=findViewById(R.id.password);
+        username=findViewById(R.id.username);
+
 
 
         login_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(Login.this, "login clicked", Toast.LENGTH_SHORT).show();
+                input_username=username.getText().toString();
+                input_password=password.getText().toString();
+                Toast.makeText(Login.this, input_username, Toast.LENGTH_SHORT).show();
+
+                if(isValidString(input_username) && isValidString(input_password)){
+                    Toast.makeText(Login.this, "logged in", Toast.LENGTH_SHORT).show();
+                    Bundle bundle=new Bundle();
+                    bundle.putString("name",input_username);
+                    Intent intent =new Intent(Login.this,AfterLoginActivity.class);
+                    intent.putExtras(bundle);
+
+                    startActivity(intent);
+                }else{
+                    username.setError("Username or password cannot be empty");
+//                    Toast.makeText(Login.this, "empty", Toast.LENGTH_SHORT).show();
+                }
+
             }
         });
         register_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Intent intent=new Intent(Login.this,RegisterActivity.class);
+                startActivity(intent);
                 Toast.makeText(Login.this, "register clicked", Toast.LENGTH_SHORT).show();
             }
         });
---
+++ AsianPay/app/src/main/java/com/example/asianpay/RegisterActivity.java
@@ -0,0 +1,25 @@
+package com.example.asianpay;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class RegisterActivity extends AppCompatActivity {
+    Button register_btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    register_btn.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent intent =new Intent(RegisterActivity.this,HomeActivity.class);
+            startActivity(intent);
+        }
+    });
+    }
+}
